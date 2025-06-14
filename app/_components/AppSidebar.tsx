7389b40b65807b1732974a84e93b2a0331db8d59
hatif03 <md.hatif0smani15@gmail.com>
2025-06-15 03:02:29 +0530

    dashboard ui changes

--- app/_components/AppSidebar.tsx
+++ app/_components/AppSidebar.tsx
@@ -11,38 +11,30 @@ import {
     SidebarMenuButton,
     SidebarMenuItem,
 } from "@/components/ui/sidebar"
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import { Home, Paintbrush, CircleDollarSign } from "lucide-react"
 import Image from 'next/image'
+import { usePathname } from 'next/navigation'
 
 const items = [
     {
-        title: "Home",
-        url: "#",
+        title: "Workspace",
+        url: "/dashboard",
         icon: Home,
     },
     {
-        title: "Inbox",
-        url: "#",
-        icon: Inbox,
+        title: "Design",
+        url: "/design",
+        icon: Paintbrush,
     },
     {
-        title: "Calendar",
-        url: "#",
-        icon: Calendar,
-    },
-    {
-        title: "Search",
-        url: "#",
-        icon: Search,
-    },
-    {
-        title: "Settings",
-        url: "#",
-        icon: Settings,
+        title: "Credits",
+        url: "/credits",
+        icon: CircleDollarSign,
     },
 ]
 
 export function AppSidebar() {
+    const path = usePathname();
     return (
         <Sidebar>
             <SidebarHeader>
@@ -60,8 +52,8 @@ export function AppSidebar() {
                             {items.map((item, index) => (
                                 // <SidebarMenuItem key={item.title} className='p-2'>
                                 //     <SidebarMenuButton asChild className=''>
-                                <a href={item.url} key={index} className='p-2 text-lg flex gap-2 items-center
-                                 hover:bg-gray-100 rounded-lg'>
+                                <a href={item.url} key={index} className={`p-2 text-lg flex gap-2 items-center
+                                 hover:bg-gray-100 rounded-lg ${path === item.url && 'bg-gray-200'}`}>
                                     <item.icon className='h-5 w-5' />
                                     <span>{item.title}</span>
                                 </a>
@@ -73,7 +65,7 @@ export function AppSidebar() {
                 </SidebarGroup>
             </SidebarContent>
             <SidebarFooter>
-                <h2 className='p-2 text-gray-400 text-sm'>Copyright @Tubeguruji</h2>
+                <h2 className='p-2 text-gray-400 text-sm'>Built with TraeAI</h2>
             </SidebarFooter>
         </Sidebar>
     )
