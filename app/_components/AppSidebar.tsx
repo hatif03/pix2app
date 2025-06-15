076b4bf9b127b8785571581286b8e7e81c52293b
hatif03 <md.hatif0smani15@gmail.com>
2025-06-15 17:49:57 +0530

    ui polishes

--- app/_components/AppSidebar.tsx
+++ app/_components/AppSidebar.tsx
@@ -11,7 +11,7 @@ import {
     SidebarMenuButton,
     SidebarMenuItem,
 } from "@/components/ui/sidebar"
-import { Home, Paintbrush, CircleDollarSign } from "lucide-react"
+import { Calendar, CircleDollarSign, Home, Inbox, Paintbrush, Search, Settings } from "lucide-react"
 import Image from 'next/image'
 import { usePathname } from 'next/navigation'
 
@@ -23,7 +23,7 @@ const items = [
     },
     {
         title: "Design",
-        url: "/design",
+        url: "/designs",
         icon: Paintbrush,
     },
     {
@@ -31,16 +31,21 @@ const items = [
         url: "/credits",
         icon: CircleDollarSign,
     },
+
 ]
 
 export function AppSidebar() {
     const path = usePathname();
+    console.log(path)
     return (
         <Sidebar>
             <SidebarHeader>
                 <div className='p-4'>
-                    <Image src={'./logo.svg'} alt='logo' width={100} height={100}
-                        className='w-full h-full' />
+                    <div className='flex items-center gap-2'>
+                        <Image src={'/logo.svg'} alt='logo' width={100} height={100}
+                            className='w-[40px] h-[40px]' />
+                        <h2 className='font-bold text-lg'>Wireframe to Code</h2>
+                    </div>
                     <h2 className='text-sm text-gray-400 text-center'>Build Awesome</h2>
                 </div>
             </SidebarHeader>
@@ -50,22 +55,22 @@ export function AppSidebar() {
                     <SidebarGroupContent>
                         <SidebarMenu className='mt-5'>
                             {items.map((item, index) => (
-                                // <SidebarMenuItem key={item.title} className='p-2'>
-                                //     <SidebarMenuButton asChild className=''>
-                                <a href={item.url} key={index} className={`p-2 text-lg flex gap-2 items-center
-                                 hover:bg-gray-100 rounded-lg ${path === item.url && 'bg-gray-200'}`}>
+                                <a href={item.url} key={index}
+                                    className={`p-2 text-lg flex gap-2 items-center
+                                 hover:bg-gray-100 rounded-lg
+                                 ${path == item.url && 'bg-gray-200'}
+                                 `}>
                                     <item.icon className='h-5 w-5' />
                                     <span>{item.title}</span>
                                 </a>
-                                //     </SidebarMenuButton>
-                                // </SidebarMenuItem>
+
                             ))}
                         </SidebarMenu>
                     </SidebarGroupContent>
                 </SidebarGroup>
             </SidebarContent>
             <SidebarFooter>
-                <h2 className='p-2 text-gray-400 text-sm'>Built with TraeAI</h2>
+                <h2 className='p-2 text-gray-400 text-sm'>Copyright @Tubeguruji</h2>
             </SidebarFooter>
         </Sidebar>
     )
